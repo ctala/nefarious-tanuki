5b902ed4cf0d9044d55961de5b0412c6fa27e717
Your Name <you@example.com>
2015-10-08 18:04:19 -0300

    Creando el plugin

--- WpSkeleton.php
+++ WpSkeleton.php
@@ -1,19 +1,37 @@
 <?php
 
 /*
-Plugin Name: Mi Plugin
-Plugin URI:  https://github.com/ctala/wp-skeleton
-Description: Base para la Creación de otros plugins
-Version:     1.0
-Author:      Cristian Tala Sánchez
-Author URI:  http://www.cristiantala.cl
-License:     MIT
-License URI: http://opensource.org/licenses/MIT
-Domain Path: /languages
-Text Domain: ctala-text_domain
-*/
+  Plugin Name: Woocommerce Compra Directa
+  Plugin URI:  https://github.com/ctala/wp-skeleton
+  Description: Plugin que permite la compra directa de los productos agregandolos al carrito y llevando a la página de checkout.
+  Version:     1.0
+  Author:      Cristian Tala Sánchez
+  Author URI:  http://www.cristiantala.cl
+  License:     MIT
+  License URI: http://opensource.org/licenses/MIT
+  Domain Path: /languages
+  Text Domain: ctala-text_domain
+ */
 include_once 'helpers/debug.php';
 
+function compraDirecta($idProducto = 8) {
+    global $woocommerce;
+    //Revisamos que el producto existe, si no existe volvemos al home.
+    //Vaciamos Carrito
+    //Agregamos al carrito
+
+
+    $woocommerce->cart->add_to_cart($idProducto);
+
+    //Reenviamos a la página de Checkout.
+    $checkout_url = $woocommerce->cart->get_checkout_url();
+    wp_redirect($checkout_url);
+    exit;
+}
+
+function agregarProductoCarrito($idProducto) {
+    
+}
 
 // Registramos los menus correspondientes
 
